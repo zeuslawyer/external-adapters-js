e5ec287aa717e5e04d2a885f0e28a9f48e9c0c2d
Thomas Hodges <thomas@smartcontract.com>
2021-12-19 16:44:34 -0600

    EthWrite: Add gasLimit to transaction set to 1000000

--- packages/targets/ethwrite/src/endpoint/txsend.ts
+++ packages/targets/ethwrite/src/endpoint/txsend.ts
@@ -24,6 +24,7 @@ const customParams = {
   dataType: false,
   result: false,
   dataToSend: false,
+  gasLimit: false,
 }
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
@@ -38,6 +39,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
   const jobRunID = validator.validated.id
   const externalAddress = validator.validated.data.exAddr
   const functionId = validator.validated.data.funcId || getUint256
+  const gasLimit = validator.validated.data.gasLimit || 1000000
   // Passing this optionally, in case the data is not encrypted from the previous step
   const dataType = validator.validated.data.dataType
   // Prioritize data coming from a previous adapter (result),
@@ -57,6 +59,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
     to: externalAddress,
     data: transactionData,
     value: 0,
+    gasLimit,
   }
 
   try {
