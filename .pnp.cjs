6e889a283db9c0664c863255589ac81a81a9ab51
bogos <andre.neyra@gmail.com>
2022-06-08 12:42:25 -0500

    add missing dependencies in coingecko

--- .pnp.cjs
+++ .pnp.cjs
@@ -4839,8 +4839,11 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@chainlink/ea-bootstrap", "workspace:packages/core/bootstrap"],
             ["@chainlink/ea-test-helpers", "workspace:packages/core/test-helpers"],
             ["@chainlink/types", "workspace:packages/core/types/@chainlink"],
+            ["@types/jest", "npm:27.0.3"],
             ["@types/node", "npm:16.11.38"],
+            ["@types/supertest", "npm:2.0.12"],
             ["nock", "npm:13.2.6"],
+            ["supertest", "npm:6.2.3"],
             ["tslib", "npm:2.3.1"],
             ["typescript", "patch:typescript@npm%3A4.3.5#~builtin<compat/typescript>::version=4.3.5&hash=493e53"]
           ],
